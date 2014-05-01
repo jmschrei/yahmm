f28b96d32ed8ebc227fba5c3cdf71da56e773777
jmschrei <jacobtribe@yahoo.com>
2014-05-01 13:24:04 -0700

    pip not actually working

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+from distutils.extension import Extension
+import numpy as np
+
+"""
+Method for importing cython modules described here:
+http://stackoverflow.com/questions/4505747/how-should-i-structure-a-
+    python-package-that-contains-cython-code
+"""
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 setup(
     name='yahmm',
-    version='0.1.1',
+    version='0.1.10',
     author='Adam Novak, Jacob Schreiber',
     author_email='anovak1@ucsc.edu, jmschreiber91@gmail.com',
     packages=['yahmm'],
@@ -11,6 +21,8 @@ setup(
     license='LICENSE.txt',
     description='HMM package which you build node by node and edge by edge.',
     long_description=open('README.txt').read(),
+    cmdclass={'build_ext':build_ext},
+    ext_modules=cythonize([ Extension( "yahmm", ["yahmm/yahmm.pyd"], include_dirs=[np.get_include()] )]),
     install_requires=[
         "cython >= 0.20.1",
         "numpy >= 1.8.0",
