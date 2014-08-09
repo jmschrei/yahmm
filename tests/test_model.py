d11519906e5f7ce68fdd5fb2b9173c7bc0f2afce
Jacob Schreiber <jacobtribe@yahoo.com>
2014-08-09 00:46:35 -0700

    expanded unit testing

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,65 +2,195 @@ from __future__ import  (division, print_function)
 
 from yahmm.yahmm import *
 from nose.tools import with_setup
+import random
+import numpy as np
 
-def setup_model():
+def setup():
+	'''
+	Build a model that we want to use to test sequences. This model will
+	be somewhat complicated, in order to extensively test YAHMM. This will be
+	a three state global sequence alignment HMM. The HMM models a reference of
+	'ACT', with pseudocounts to allow for slight deviations from this
+	reference.
+	'''
 
 	random.seed(0)
 
-	s1 = State(UniformDistribution(0.0, 1.0), name="S1")
-	s2 = State(UniformDistribution(0.5, 1.5), name="S2")
-	s3 = State(UniformDistribution(-1.0, 1.0), name="S3")
+	global model
+	model = Model( "Global Alignment")
 
-	# Make a simple 2-state model
-	global model_a
-	model_a = Model(name="A")
-	model_a.add_state(s1)
-	model_a.add_state(s2)
-	model_a.add_transition(s1, s1, 0.70)
-	model_a.add_transition(s1, s2, 0.25)
-	model_a.add_transition(s1, model_a.end, 0.05)
-	model_a.add_transition(s2, s2, 0.70)
-	model_a.add_transition(s2, s1, 0.25)
-	model_a.add_transition(s2, model_a.end, 0.05)
-	model_a.add_transition(model_a.start, s1, 0.5)
-	model_a.add_transition(model_a.start, s2, 0.5)
+	# Define the distribution for insertions
+	i_d = DiscreteDistribution( { 'A': 0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25 } )
 
-	# Make another model with that model as a component
-	global model_b
-	model_b = Model(name="B")
-	model_b.add_state(s3)
-	model_b.add_transition(model_b.start, s3, 1.0)
-	model_b.add_model(model_a)
-	model_b.add_transition(s3, model_a.start, 1.0)
-	model_b.add_transition(model_a.end, model_b.end, 1.0)
-	model_b.bake()
+	# Create the insert states
+	i0 = State( i_d, name="I0" )
+	i1 = State( i_d, name="I1" )
+	i2 = State( i_d, name="I2" )
+	i3 = State( i_d, name="I3" )
 
-def teardown_model():
-	pass # just pass for now
+	# Create the match states
+	m1 = State( DiscreteDistribution({ "A": 0.95, 'C': 0.01, 'G': 0.01, 'T': 0.02 }) , name="M1" )
+	m2 = State( DiscreteDistribution({ "A": 0.003, 'C': 0.99, 'G': 0.003, 'T': 0.004 }) , name="M2" )
+	m3 = State( DiscreteDistribution({ "A": 0.01, 'C': 0.01, 'G': 0.01, 'T': 0.97 }) , name="M3" )
 
-@with_setup(setup_model, teardown_model)
-def test_sampling():
+	# Create the delete states
+	d1 = State( None, name="D1" )
+	d2 = State( None, name="D2" )
+	d3 = State( None, name="D3" )
 
-	sample = model_b.sample()
+	# Add all the states to the model
+	model.add_states( [i0, i1, i2, i3, m1, m2, m3, d1, d2, d3 ] )
 
-	assert all([val in sample for val in [0.515908805880605, 1.0112747213686086,
-		1.2837985890347725, 0.9765969541523558, 1.4081128851953353,
-		0.7818378443997038, 0.6183689966753316]]) , "Sampling check"
+	# Create transitions from match states
+	model.add_transition( model.start, m1, 0.9 )
+	model.add_transition( model.start, i0, 0.1 )
+	model.add_transition( m1, m2, 0.9 )
+	model.add_transition( m1, i1, 0.05 )
+	model.add_transition( m1, d2, 0.05 )
+	model.add_transition( m2, m3, 0.9 )
+	model.add_transition( m2, i2, 0.05 )
+	model.add_transition( m2, d3, 0.05 )
+	model.add_transition( m3, model.end, 0.9 )
+	model.add_transition( m3, i3, 0.1 )
 
-@with_setup(setup_model, teardown_model)
-def test_viterbi():
-	# This sequence should work
-	viterbi_output = model_b.viterbi([-0.5, 0.2, 0.2])
+	# Create transitions from insert states
+	model.add_transition( i0, i0, 0.70 )
+	model.add_transition( i0, d1, 0.15 )
+	model.add_transition( i0, m1, 0.15 )
 
-	assert viterbi_output[0] == -4.738701578612614, "Viterbi check"
+	model.add_transition( i1, i1, 0.70 )
+	model.add_transition( i1, d2, 0.15 )
+	model.add_transition( i1, m2, 0.15 )
 
-	# This one should not
-	viterbi_output = model_b.viterbi([-0.5, 0.2, 0.2 -0.5])
-	assert str(viterbi_output) == "(-inf, None)", "Impossible sequence Viterbi check"
+	model.add_transition( i2, i2, 0.70 )
+	model.add_transition( i2, d3, 0.15 )
+	model.add_transition( i2, m3, 0.15 )
 
-@with_setup(setup_model, teardown_model)
-def test_training():
-	training_improvement = model_b.train([[-0.5, 0.2, 0.2], [-0.5, 0.2, 1.2, 0.8]],
-		                                 transition_pseudocount=1)
+	model.add_transition( i3, i3, 0.85 )
+	model.add_transition( i3, model.end, 0.15 )
 
-	assert str(training_improvement) == str(4.13320746579), "Model training improvement"
+	# Create transitions from delete states
+	model.add_transition( d1, d2, 0.15 )
+	model.add_transition( d1, i1, 0.15 )
+	model.add_transition( d1, m2, 0.70 ) 
+
+	model.add_transition( d2, d3, 0.15 )
+	model.add_transition( d2, i2, 0.15 )
+	model.add_transition( d2, m3, 0.70 )
+
+	model.add_transition( d3, i3, 0.30 )
+	model.add_transition( d3, model.end, 0.70 )
+
+	# Call bake to finalize the structure of the model.
+	model.bake()
+
+def teardown():
+	'''
+	Remove the model at the end of the unit testing. Since it is stored in a
+	global variance, simply delete it.
+	'''
+
+	pass
+
+@with_setup( setup, teardown )
+def test_same_length_viterbi():
+	'''
+	Take a few sequences of the same length of the profile, and ensure that
+	the log probability of the Viterbi path equals the predetermined values.
+	'''
+
+	scores = [ -0.5132449003570658, -11.048101241343396, -9.125519674022627, 
+		-5.0879558788604475 ]
+	sequences = map( list, [ 'ACT', 'GGC', 'GAT', 'ACC' ] )
+	
+	for seq, score in zip( sequences, scores ):
+		assert model.viterbi( seq )[0] == score, \
+			"Same length Viterbi check on Sequence '{}'".format( seq )
+
+	assert str( model.viterbi( list('XXX') ) ) == "(-inf, None)", \
+		"Impossible sequence Viterbi check"
+
+@with_setup( setup, teardown )
+def test_variable_length_viterbi():
+	'''
+	Take a few sequence of variable length, and ensure that the log probability
+	of the Viterbi path equals the predetermined values.
+	'''
+
+	scores = [ -5.406181012423981, -10.88681993576597, -3.6244718790494277, 
+	-3.644880750680635, -10.674332964640293, -10.393824835172445, 
+	-8.67126440174503, -16.903451796110275, -16.451699654050792 ]
+	sequences = map( list, ('A', 'GA', 'AC', 'AT', 'ATCC', 
+		'ACGTG', 'ATTT', 'TACCCTC', 'TGTCAACACT') )
+
+	for seq, score in zip( sequences, scores ):
+		assert model.viterbi( seq )[0] == score, \
+			"Variable length Viterbi check on Sequence '{}'".format( seq )
+
+@with_setup( setup, teardown )
+def test_log_probability():
+	'''
+	Take a few sequences of variable length, and ensure that the sum of all
+	paths log probability equals the predetermined values.
+	'''
+
+	scores = [ -5.3931, -0.5052, -11.8478, -14.3482 ]
+	sequences = map( list, ( 'A', 'ACT', 'GGCA', 'TACCTGT' ) )
+
+	for seq, score in zip( sequences, scores ):
+		print( round( model.log_probability( seq ), 4 ) )
+		assert round( model.log_probability( seq ), 4 ) == score, \
+			"Variable length log probability check on '{}'".format( seq )
+
+@with_setup( setup, teardown )
+def test_posterior_transitions():
+	'''
+	Take a few sequences of variable length, and ensure that some posterior
+	decodings work.
+	'''
+
+	a_scores = [ 0.0, 0.0021, 0.2017, 1.5105 ]
+	b_scores = [ 0.013, 0.0035, 0.817, 0.477 ]
+	t_scores = [ 4.013, 4.0083, 6.457, 8.9812 ]
+	sequences = map( list, ( 'A', 'ACT', 'GGCA', 'TACCTGT' ) )
+
+	indices = { state.name: i for i, state in enumerate( model.states ) }
+	i, j = indices['I2'], indices['D1']
+
+	for seq, a, b, t in zip( sequences, a_scores, b_scores, t_scores ):
+		trans, ems = model.forward_backward( seq )
+
+		assert round( trans[i].sum(), 4 ) == a, \
+			"Posterior transitions incorrect for '{}'".format( seq )
+		assert round( trans[j].sum(), 4 ) == b, \
+			"Posterior transitions incorrect for '{}'".format( seq )
+		assert round( trans.sum(), 4 ) == t, \
+			"Posterior transitions incorrect for '{}'".format( seq )
+
+@with_setup( setup, teardown )
+def test_posterior_emissions():
+	'''
+	Take a few sequences of variable length, and ensure that some posterior
+	decodings work.
+	'''
+
+	a_scores = [ 0.987, 0.9965, 0.183, 0.523 ]
+	b_scores = [ 0.0, 0.9977, 0.7364, 0.6318 ]
+	c_scores = [ 0.0, 0.9975, 0.6237, 0.8641 ]
+	sequences = map( list, ( 'A', 'ACT', 'GGCA', 'TACCTGT' ) )
+
+	indices = { state.name: i for i, state in enumerate( model.states ) }
+	i, j, k = indices['M1'], indices['M2'], indices['M3']
+
+	for seq, a, b, c in zip( sequences, a_scores, b_scores, c_scores ):
+		trans, ems = model.forward_backward( seq )
+		ems = np.exp( ems )
+
+		assert round( ems[:,i].sum(), 4 ) == a, \
+			"Posterior emissions incorrect for '{}'".format( seq )
+		assert round( ems[:,j].sum(), 4 ) == b, \
+			"Posterior emissions incorrect for '{}'".format( seq )
+		assert round( ems[:,k].sum(), 4 ) == c, \
+			"Posterior emissions incorrect for '{}'".format( seq )
+		assert round( ems.sum() ) == len( seq ), \
+			"Posterior emissions incorrect for '{}'".format( seq )
